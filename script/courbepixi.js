0ca9a98684055eea84b2219164275a0858d4f68a
Joseph <kaluznyadr@outlook.fr>
2023-10-04 16:56:44 +0200

    Ajout timeline + click point apprait text + image sans style

--- script/courbepixi.js
+++ script/courbepixi.js
@@ -25,6 +25,8 @@ const viewport = new Viewport({
 app.stage.addChild(viewport);
 viewport.drag().pinch().wheel().decelerate();
 
+let infoText = null; // Variable to hold info text
+let infoImage = null;
 fetch('Json/music.json')
     .then(response => response.json())
     .then(timelineData => {
@@ -48,9 +50,8 @@ fetch('Json/music.json')
         const centerX = (timelineContainer.width - lineWidth) / 2; // Calculate the center X position
         
         myGraph.lineStyle(3, 0x000000);
-myGraph.moveTo(centerX, 0);
-myGraph.lineTo(centerX + lineWidth, 0);
-
+        myGraph.moveTo(centerX, 0);
+        myGraph.lineTo(centerX + lineWidth, 0);
 
         for (const entry of allEntries) {
             if (entry.subgenre) {
@@ -64,32 +65,62 @@ myGraph.lineTo(centerX + lineWidth, 0);
         allEntries.sort((a, b) => a.date - b.date);
 
         for (const entry of allEntries) {
-        const x = parseInt(entry.date);
+            const x = parseInt(entry.date);
 
-        const point = new PIXI.Graphics();
-        point.beginFill(0xFF0000);
-        point.drawCircle(0, 0, 5);
+            const point = new PIXI.Graphics();
+            point.beginFill(0xFF0000);
+            point.drawCircle(0, 0, 5);
 
-        point.interactive = true;
-        point.buttonMode = true;
+            point.interactive = true;
+            point.buttonMode = true;
 
+            point.on("click", () => {
 
-        point.on("click", () => {
-            showDescription(entry.genre, entry.description, entry.image);
-        });
+                timelineContainer.removeChild(infoText);
+                timelineContainer.removeChild(infoImage);
+                // Create new info text and add it to the timeline container
+                infoText = new PIXI.Text(entry.description, {
+                    fontFamily: 'Arial',
+                    fontSize: 16,
+                    fill: 0x000000,
+                    wordWrap: true,         // Enable word wrapping
+                    wordWrapWidth: 300,     // Set the maximum width in pixels
+                    lineHeight: 24,         // Adjust line height as needed
+                });
+                infoText.anchor.set(0.5, 0);
+                infoText.x = point.x;
+                infoText.y = 60; // Adjust vertical position as needed
 
+                const texture = PIXI.Texture.from(entry.image);
+                infoImage = new PIXI.Sprite(texture);
+                const targetWidth = 20;
+                const targetHeight = 20;
+                const scale = Math.min(targetWidth, targetHeight);
+
+                // Apply the calculated scale to the image
+               
+                // Create a PIXI.Sprite using the texture
+                
+                infoImage.scale.set(0.5, 0.5);
+                infoImage.anchor.set(0.5, 0);
+                infoImage.x = point.x;
+                infoImage.y = 30;
+
+                timelineContainer.addChild(infoImage);
+                timelineContainer.addChild(infoText);
+            });
+            
             const dateText = new PIXI.Text(entry.date, { fontSize: 12, fill: 0x000000 });
             dateText.anchor.set(0.5, 0);
             dateText.x = 0;
-            dateText.y = 10;
+            dateText.y = -30;
 
             point.addChild(dateText);
             const dateTitreText = new PIXI.Text(entry.genre, { fontSize: 16, fill: 0x000000 });
             dateTitreText.anchor.set(0.5, 0);
             dateTitreText.x = 0;
-            dateTitreText.y = 40;
+            dateTitreText.y = -60;
             point.addChild(dateTitreText);
-
 
             point.x = xPosition;
             xPosition += 200;
@@ -98,43 +129,12 @@ myGraph.lineTo(centerX + lineWidth, 0);
         }
 
         // Add the timeline container to the viewport and center it horizontally
-viewport.addChild(timelineContainer);
-timelineContainer.y = (viewport.worldHeight - totalHeight) / 2;
-timelineContainer.x = (viewport.worldWidth - totalWidth) / 2;
+        viewport.addChild(timelineContainer);
+        timelineContainer.y = (viewport.worldHeight - totalHeight) / 2;
+        timelineContainer.x = (viewport.worldWidth - totalWidth) / 2;
 
- // Center the timeline
+        // Center the timeline
     })
     .catch(error => {
         console.error('Error fetching JSON data:', error);
     });
-
-
-    document.getElementById("closeModal").addEventListener("click", closeModal);
-
-    function showDescription(genre, description, image) {
-        const modal = document.getElementById("modal");
-        const modalTitle = document.getElementById("modalTitle");
-        const modalDescription = document.getElementById("modalDescription");
-        const modalImage = document.getElementById("modalImage");
-    
-        modalTitle.textContent = genre;
-        modalDescription.textContent = description;
-        modalImage.src = image;
-    
-        modal.style.display = "block";
-    }
-    //Could it be possible to make it appear and still be able to move around and close the modal on touch of a cross or if i open another point
-    function closeModal() {
-        const modal = document.getElementById("modal");
-        modal.style.display = "none";
-    }
-
-document.body.addEventListener("click", () => {
-    // console.log(viewport.scale);
-});
-
-
-///////////////////////////////////:::
-
-
-
